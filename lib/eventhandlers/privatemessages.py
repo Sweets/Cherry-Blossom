ea3c79cb0e23bf0b39ef1cd29a55c7861399a86b
Sweets <Sweets@users.noreply.github.com>
2019-06-01 00:58:43 -0500

    Update privatemessages.py

--- lib/eventhandlers/privatemessages.py
+++ lib/eventhandlers/privatemessages.py
@@ -3,17 +3,95 @@ import time
 
 from ..message import PrivateMessage as Message
 
+async def seller_name(client, user_name, session_id):
+    pass
+
 async def _time(client, *args):
-    print(*args)
+    pass
 
 async def OK(client):
+    await client.send_frame("wl")
     await client.send_frame("getblock")
     await client.emit_event("private_messages_connected")
 
+async def denied(client):
+    await client.emit_event("private_messages_denied")
+    
 async def premium(client, flags, _time):
     if int(_time) > time.time():
         await client.send_frame("msgbg", 1)
 
+async def wl(client, *friend_list):
+    if len(client.friends) >= 1:
+        client.friends = list()
+    
+    for index in range(0, len(friend_list), 4):
+        friend_data = friend_list[index:index + 4]
+        friend = Friend(client, *friend_data)
+        
+        client.friends.append(friend)
+        await client.send_frame("track", friend.name)
+
+async def wladd(client, user_name, status, _time):
+    if status == "invalid":
+        return
+    
+    friend = await client.get_friend(user_name)
+    if not friend:
+        friend = Friend(client, user_name, time.time(), status, _time)
+        client.friends.append(friend)
+        
+        await client.emit_event("private_messages_friend_added", friend)
+        await client.send_frame("wl")
+        await client.send_frame("track", user_name.lower())
+
+async def wldelete(client, user_name, status, unknown):
+    friend = await client.get_friend(user_name)
+    if friend and status == "deleted":
+        for _friend in client.friends:
+            if _friend.name == friend.name:
+                client.friends.remove(_friend)
+                await client.emit_event("private_messages_friend_deleted", user_name)
+
+async def wlonline(client, user_name, _time):
+    pass # Users that are set to be tracked (track frame) are output in status
+    # No need to take advantage of wlonline, since all users are tracked
+
+async def wloffline(client, user_name, _time):
+    pass
+
+async def wlapp(client, user_name, _time):
+    pass
+
+async def status(client, user_name, _time, status, unknown):
+    friend = await client.get_friend(user_name)
+    if friend:
+        friend.last_active = float(_time)
+        friend.idle = False
+        
+        if status == "online":
+            friend.status = "on"
+            await client.emit_event("private_messages_friend_online", friend)
+        else:
+            friend.status = "off" if status == "offline" else status
+            await client.emit_event("private_messages_friend_offline", friend)
+
+async def track(client, user_name, _time, status):
+    friend = Friend(client, user_name, _time, status, _time)
+    
+    if status == "online":
+        friend.last_active = time.time() - (int(_time) * 60)
+    elif status == "offline":
+        friend.last_active = float(_time)
+    
+    await client.emite_event("private_messages_friend_added", friend)
+
+async def idleupdate(client, user_name, idle_status):
+    friend = await client.get_friend(user_name)
+    if friend:
+        friend.idle = idle_status == "0"
+        friend.last_active = time.time()
+    
 async def msg(client, *args):
     await client.emit_event("private_messages_message_received", Message(*args))
 
