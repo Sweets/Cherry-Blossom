102b7463ad2249eb4fce653dae49f0103479d0b3
Sweets <Sweets@users.noreply.github.com>
2019-06-01 02:40:11 -0500

    Added get_friend, add_friend, remove_friend, better message formatting

--- lib/privatemessages.py
+++ lib/privatemessages.py
@@ -5,6 +5,10 @@ privatemessages.py
 
 import asyncio
 
+from sys import getsizeof
+
+from .chatango.utilities import user_name_is_valid
+from .chatango.constants import FONT_FACES, MAX_MESSAGE_SIZE
 from .eventhandlers import privatemessages as eventhandler
 from .transport import Transport
 
@@ -46,11 +50,43 @@ class PrivateMessages(Transport):
     async def disable_background(self):
         await self.send_frame("msgbg", 0)
 
-    async def send_message(self, *args, recipient=None):
-        if isinstance(recipient, str): # TO-DO: verify username
-            await self.send_frame("msg", recipient.lower(),
-                                  "<n" + self.user.styles.name_color + "/><m v=\"1\"><g x" +
-                                  self.user.styles.font_size + "s" +
-                                  self.user.styles.font_color + "=\"" +
-                                  self.user.styles.font_face + "\">" + " ".join(args) + "</g></m>"
-                                 )
+    async def get_friend(self, user_name):
+        assert user_name_is_valid(user_name), "Invalid user name"
+        for friend in self.friends:
+            if friend.name == user_name.lower():
+                return friend
+
+    async def add_friend(self, user_name):
+        assert user_name_is_valid(user_name), "Invalid user name"
+        friend = await self.get_friend(user_name)
+        if not friend:
+            await self.send_frame("wladd", user_name.lower())
+
+    async def remove_friend(self, user_name):
+        assert user_name_is_valid(user_name), "Invalid user name"
+        friend = await self.get_friend(user_name)
+        if friend:
+            await self.send_frame("wldelete", friend.name)
+
+    async def send_message(self, *message, recipient=None):
+        assert user_name_is_valid(recipient), "Invalid user name"
+        
+        if len(message) > 0:
+            name_color = "<n{}/>".format(self.user.styles.name_color)
+            
+            font_size = self.user.styles.font_size if font_size > 10 else "09"
+            font_face = self.user.styles.font_family
+            font_face = FONT_FACES.get(font_face, font_face)
+            
+            message = "<br/>".join(message)
+            
+            message_xml = "<m v=\"1\"><g x{}s{}=\"{}\">{}</g></m>"
+            format_parameters = (font_size,
+                                 self.user.styles.font_color,
+                                font_face,
+                                message)
+            
+            message = name_color + message_xml.format(*format_parameters)
+            
+            if getsizeof(message) < MAX_MESSAGE_SIZE:
+                await self.send_frame("msg", recipient.lower(), message)
